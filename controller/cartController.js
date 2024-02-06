9368955adaf5c0139765fff94a775ef96ac6345a
Ray259 <elninothepea@gmail.com>
2024-02-06 22:52:47 +0700

    server

--- controller/cartController.js
+++
@@ -1,8 +0,0 @@
-const { Cart } = require("../models");
-const sequelize = require("sequelize");
-
-module.exports.view = async (req, res) => {
-    const id = req.params.id;
-    const cart = await Cart.findAll({ where: { fk_user: id } });
-    res.json(cart);
-};
